3a952d98f65e5770d2ff0f70b76f13178c278916
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-11-17 01:18:19 +0000

    Explanation of how to break down this exercise
    Using IRB we get nil for the puts line...
    But that is because puts always returns nil.
    
    Also, the evaluation of the line is separate from any assignments that
    happen.  So we can both take advantage of this, as we may see in a
    future week, but also we should be aware that every line in Ruby returns
    something.

--- 1_week/1e_operator_precedence.rb
+++ 1_week/1e_operator_precedence.rb
@@ -1,3 +1,6 @@
+# evaluating this in IRB opens our eyes to the returns of the lines.
+# In other words, what the line evalutes as, which may be different than any
+# assignments.
 y = false
 z = true
 x = y or z
